c195783b560e786a65d39c0c65e015b6e0345bc1
Super Villain <anikethgandhari145@gmail.com>
2023-09-05 11:02:03 +0530

    Optimised code, added new routes and styles

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-// NPM Require
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
-// Local Require
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+  require("dotenv").load();
 }
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
-const app = express();
-
-// Middleware
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
-
-// Templating Engine (EJS)
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-// Related to MongoDB
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-});
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connected to DB"));
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.DATABASE_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to DB");
+  }
+);
 
-// Using Routes
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
-// Server Listen
-app.listen(process.env.PORT || 5000, () => console.log("Running Server..."));
+app.listen(process.env.PORT || 5000);
