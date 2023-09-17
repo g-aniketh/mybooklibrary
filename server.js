f26f8e9c60530ab9b0f5dc1cee1e6757b1473bba
Super Villain <anikethgandhari145@gmail.com>
2023-09-17 13:02:33 +0530

    Development Dependency Changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").load();
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -25,7 +25,7 @@ mongoose.connect(
   process.env.DATABASE_URL,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   },
   () => {
     console.log("Connected to DB");
